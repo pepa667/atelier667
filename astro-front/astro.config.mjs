596e189e811412bb74a6d570c6c6112c1c296c88
Pepa <9425683+pepa667@users.noreply.github.com>
2026-07-09 04:47:06 -0300

    Iniciando

--- astro-front/astro.config.mjs
+++ astro-front/astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    // Força o Vite a ignorar o cache quebrado de resolução de caminhos na Netlify
+    resolve: {
+      preserveSymlinks: true,
+    },
   },
-
-  integrations: [react()]
+  integrations: [react()],
 });
