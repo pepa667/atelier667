f0f7e34ced7d823d423dcf01ca360c7f2ced6c2b
Pepa <9425683+pepa667@users.noreply.github.com>
2026-07-09 04:42:17 -0300

    Iniciando

--- astro-front/src/lib/sanity.js
+++ astro-front/src/lib/sanity.js
@@ -1,8 +1,8 @@
-import { createClient } from '@sanity/client';
+import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-    projectId: '7ev62ugl', // <--- IMPORTANTE: Copie o ID do seu projeto no painel do Sanity
-    dataset: 'production',
-    useCdn: false, // false garante que a gente sempre puxe o dado mais fresco da bancada
-    apiVersion: '2026-06-21', // Data de hoje (padrão de versionamento da API deles)
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID, // <--- Agora lê do .env
+  dataset: "production",
+  useCdn: false, // false garante que a gente sempre puxe o dado mais fresco da bancada
+  apiVersion: "2026-06-21", // Data de hoje (padrão de versionamento da API deles)
 });
