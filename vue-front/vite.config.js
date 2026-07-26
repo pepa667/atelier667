c2d7a17d97f27170783b6d9ed60c47383bb32b00
Pepa <9425683+pepa667@users.noreply.github.com>
2026-07-26 10:59:57 -0300

    Tentando

--- vue-front/vite.config.js
+++ vue-front/vite.config.js
@@ -4,4 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Divide bibliotecas pesadas de terceiros em chunks separados
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
